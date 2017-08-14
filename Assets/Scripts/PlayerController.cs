ff598feb562e79e14ebe625a70a4b814d7aa6d47
Steve Barskey <steve@iMac.local>
2017-08-13 20:09:01 -0700

    Initial commit.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+
+	private Animator anim;
+	private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+		anim = gameObject.GetComponent<Animator> ();
+
+		rb2d = gameObject.GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float inputX = Input.GetAxisRaw ("Horizontal");
+		float inputY = Input.GetAxisRaw ("Vertical");
+
+		anim.SetFloat ("SpeedX", inputX);
+		anim.SetFloat ("SpeedY", inputY);
+
+		rb2d.velocity = new Vector2 (inputX * speed, inputY * speed);
+	}
+}
