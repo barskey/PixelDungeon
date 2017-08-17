5dbd1ef1b855b8dfe07510f76621c2a50fbbf561
barskey <barskey@gmail.com>
2017-08-16 23:11:32 -0700

    Patrol points started.

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class EnemyController : MonoBehaviour {
+
+	public float maxSpeed = 1f;
+	public float patrolSpeed = 0.5f;
+
+	private Rigidbody2D rb2d;
+	private int patrolIndex = 0;
+	private List<GameObject> patrolPoints = new List<GameObject> ();
+
+	// Use this for initialization
+	void Start ()
+	{
+		rb2d = gameObject.GetComponent<Rigidbody2D> ();
+
+		// find all patrol points (tagged with "patrolpoint" and sort the list by name
+		patrolPoints = GameObject.FindGameObjectsWithTag ("PatrolPoint").OrderBy(point=>point.name).ToList();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Patrol ();
+
+		// if close to current patrol point, incrememnt index
+		if (Vector3.Distance (transform.position, patrolPoints [patrolIndex].transform.position) < 0.02f) {
+			patrolIndex++;
+			if (patrolIndex >= patrolPoints.Count())
+				patrolIndex = 0;
+		}
+	}
+
+	void Patrol ()
+	{
+		Vector3 moveDir = patrolPoints [patrolIndex].transform.position - transform.position;
+		moveDir.Normalize();
+
+		rb2d.velocity = new Vector2 (moveDir.x * patrolSpeed, moveDir.y * patrolSpeed);
+	}
+}
---
+++ Assets/Scripts/SpawnController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour {
+
+	public GameObject enemy;
+	public float spawnDelay = 3f;
+	public int maxSpawn = 3;
+	public float health = 100f;
+
+	void Awake()
+	{
+		InvokeRepeating ("SpawnEnemy", 1f, spawnDelay);
+	}
+
+	void SpawnEnemy()
+	{
+		if (GameObject.FindGameObjectsWithTag ("Enemy").Length < maxSpawn) {
+			Debug.Log ("Spawning enemy...");
+			GameObject.Instantiate (enemy, gameObject.transform.position, Quaternion.identity);
+		}
+	}
+}
