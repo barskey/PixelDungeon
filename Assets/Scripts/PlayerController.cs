4e77cf3829050e0d0a62f7004720a2771012077e
Steve Barskey <steve@iMac.local>
2017-08-21 20:11:16 -0700

    Added state machines.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,13 +10,15 @@ public class PlayerController : MonoBehaviour {
 	private Rigidbody2D rb2d;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		anim = gameObject.GetComponent<Animator> ();
 		rb2d = gameObject.GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 		float inputX = Input.GetAxisRaw ("Horizontal");
 		float inputY = Input.GetAxisRaw ("Vertical");
 
@@ -24,5 +26,33 @@ public class PlayerController : MonoBehaviour {
 		anim.SetFloat ("SpeedY", inputY);
 
 		rb2d.velocity = new Vector2 (inputX * speed, inputY * speed);
+	}
+
+	void FixedUpdate ()
+	{
+		float lastInputX = Input.GetAxisRaw ("Horizontal");
+		float lastInputY = Input.GetAxisRaw ("Vertical");
+
+		if (lastInputX != 0 || lastInputY != 0) {
+			anim.SetBool ("Walking", true);
+
+			if (lastInputX > 0) {
+				anim.SetFloat ("LastMoveX", 1f);
+			} else if (lastInputX < 0) {
+				anim.SetFloat ("LastMoveX", -1f);
+			} else {
+				anim.SetFloat ("LastMoveX", 0);
+			}
+
+			if (lastInputY > 0) {
+				anim.SetFloat ("LastMoveY", 1f);
+			} else if (lastInputY < 0) {
+				anim.SetFloat ("LastMoveY", -1f);
+			} else {
+				anim.SetFloat ("LastMoveY", 0);
+			}
+		} else {
+			anim.SetBool ("Walking", false);
+		}
 	}
 }
